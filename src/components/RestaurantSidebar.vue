df173f17e21cf75ff4694cdcc2f37d6f6ad5fdf8
AleZadik <awzadik@gmail.com>
2022-10-16 19:10:24 -0300

    Save Current Views

---
+++ src/components/RestaurantSidebar.vue
@@ -0,0 +1,26 @@
+<!-- List of Restaurants -->
+<template>
+    <p>Test</p>
+</template>
+
+<script>
+export default{
+    name: 'RestaurantSidebar',
+    props: {
+        lat: {
+            type: Number,
+            default: 0
+        },
+        lng: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            map: null,
+            marker: null
+        }
+    },
+}
+</script>
--- src/views/EstablishmentView.vue
+++ src/views/EstablishmentView.vue
@@ -1,126 +1,39 @@
 <template>
-    <ConfirmDialog></ConfirmDialog>
-    <Sidebar v-model:visible="visibleLeft" :dismissable="false" :modal="false" :showCloseIcon="true">
-        <div class="p-d-flex p-flex-column p-ai-center p-mt-4">
-            <h1 class="p-mb-2">Restaurants near me</h1>
-            <div class="p-d-flex p-flex-column p-ai-center p-mt-4">
-                <div class="p-d-flex p-flex-row p-ai-center p-mb-2">
-                    <Button icon="pi pi-plus" class="p-button-rounded p-button-success p-mr-2" @click="addRestaurantDialog = true"></Button>
-                    <h3>Add restaurant</h3>
+    <div class="container">
+        <div class="row">
+            <Sidebar class="pct15" v-model:visible="visibleLeft" :dismissable="false" :modal="false"
+                :showCloseIcon="false">
+                <template #header>
+                    <div class="p-d-flex p-flex-row p-ai-center p-mt-4">
+                        <h1 class="title"><img src="../assets/logo.svg" alt="logo" class="logo" />FoodCircle</h1>
+                    </div>
+                </template>
+                <div class="p-d-flex p-flex-column p-ai-center p-mt-4">
+                    <div class="p-d-flex p-flex-column p-ai-center p-mt-4">
+                        <div class="item">
+                            <div class="option"><span>Dashboard</span></div>
+                        </div>
+                        <div class="item">
+                            <div class="option"><span>Timeline</span></div>
+                        </div>
+                        <div class="item">
+                            <div class="option" @click="test"><span>Establishment</span></div>
+                        </div>
+                    </div>
                 </div>
-                <div class="p-d-flex p-flex-row p-ai-center p-mb-2">
-                    <Button icon="pi pi-plus" class="p-button-rounded p-button-success p-mr-2" @click="addRestaurantDialog = true"></Button>
-                    <h3>Add restaurant</h3>
-                </div>
-                <div class="p-d-flex p-flex-row p-ai-center p-mb-2">
-                    <Button icon="pi pi-plus" class="p-button-rounded p-button-success p-mr-2" @click="addRestaurantDialog = true"></Button>
-                    <h3>Add restaurant</h3>
-                </div>
-            </div>
-        </div>
-    </Sidebar>
-
-    <Button icon="pi pi-arrow-right" @click="visibleLeft = true" />
-    <div>
-        <h5>Addons</h5>
-        <div class="grid p-fluid">
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">
-                        <i class="pi pi-user"></i>
-                    </span>
-                    <InputText placeholder="Username" />
-                </div>
-            </div>
-
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">$</span>
-                    <InputText placeholder="Price" />
-                    <span class="p-inputgroup-addon">.00</span>
-                </div>
-            </div>
-
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">W</span>
-                    <InputText placeholder="Website" />
-                </div>
-            </div>
-        </div>
-
-        <h5>Multiple Addons</h5>
-        <div class="grid">
-            <div class="col-12">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">
-                        <i class="pi pi-clock"></i>
-                    </span>
-                    <span class="p-inputgroup-addon">
-                        <i class="pi pi-star-fill"></i>
-                    </span>
-                    <InputText placeholder="Price" />
-                    <span class="p-inputgroup-addon">$</span>
-                    <span class="p-inputgroup-addon">.00</span>
-                </div>
-            </div>
-        </div>
-
-        <h5>Button Addons</h5>
-        <div class="grid p-fluid">
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <Button label="Search" />
-                    <InputText placeholder="Keyword" />
-                </div>
-            </div>
-
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <InputText placeholder="Keyword" />
-                    <Button icon="pi pi-search" class="p-button-warning" />
-                </div>
-            </div>
-
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <Button icon="pi pi-check" class="p-button-success" />
-                    <InputText placeholder="Vote" />
-                    <Button icon="pi pi-times" class="p-button-danger" />
-                </div>
-            </div>
-        </div>
-
-        <h5>Checkbox and RadioButton</h5>
-        <div class="grid p-fluid">
-            <div class="col-12 md:col-12">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">
-                        <Checkbox v-model="checked1" :binary="true" />
-                    </span>
-                    <InputText placeholder="Username" />
-                </div>
-            </div>
-
-            <div class="col-12 md:col-12">
-                <div class="p-inputgroup">
-                    <InputText placeholder="Price" />
-                    <span class="p-inputgroup-addon">
-                        <RadioButton name="rb1" value="rb1" v-model="radioValue1" />
-                    </span>
-                </div>
-            </div>
-
-            <div class="col-12 md:col-12">
-                <div class="p-inputgroup">
-                    <span class="p-inputgroup-addon">
-                        <Checkbox v-model="checked2" :binary="true" />
-                    </span>
-                    <InputText placeholder="Website" />
-                    <span class="p-inputgroup-addon">
-                        <RadioButton name="rb2" value="rb2" v-model="radioValue2" />
-                    </span>
-                </div>
+            </Sidebar>
+            <div class="pct15"></div>
+            <div class="pct85">
+                <Menubar :model="items" class="fullwidth-sidebar">
+                    <template #start>
+                        <img alt="logo" src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+                            height="40" class="mr-2">
+                    </template>
+                    <template #end>
+                        <InputText placeholder="Search" type="text" />
+                    </template>
+                </Menubar>
+                <div id="content"></div>
             </div>
         </div>
     </div>
@@ -146,10 +59,95 @@ export default {
     mounted() {
     },
     methods: {
+        test(){
+            console.log("test");
+        }
     },
 }
 
 </script>
-
 <style>
+.title {
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: #000;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+}
+
+.p-sidebar-header {
+    background-color: #fff;
+    border-bottom: 1px solid #ebebeb;
+    height: 10vh;
+}
+
+.logo {
+    width: 50px;
+    height: 50px;
+}
+
+.p-sidebar-header-content {
+    background-color: #f5f5f5;
+    width: 100%;
+    height: 100%;
+}
+
+.p-sidebar-content {
+    padding: 0 !important;
+}
+
+.item {
+    font-size: 1.2rem;
+    color: #000;
+    margin: 0;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    width: 100%;
+    border-bottom: 1px solid #ebebeb;
+}
+
+.left-data {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 1rem;
+}
+
+.store-summary {
+    height: 300px;
+}
+
+.option {
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+
+.pct85 {
+    width: 85vw;
+    height: 100vh;
+    float: right;
+    background-color: rgb(255, 255, 255);
+}
+
+.pct15 {
+    width: 15vw !important;
+    height: 100vh !important;
+    float: left;
+    background-color: #17212F;
+    transition: none !important;
+}
+
+.fullwidth-sidebar {
+    width: 100% !important;
+    background: #f8f9fa !important;
+    border: none !important;
+    border-bottom: gray solid 1px !important;
+    border-radius: 0 !important;
+}
 </style>
